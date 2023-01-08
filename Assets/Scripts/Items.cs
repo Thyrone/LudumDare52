5ab374ec3f36d450881190375d4b32bd76b58926
DESKTOP-TPTUSH0\Jean <jean.zwolinski@edu.gobelins.fr>
2023-01-08 16:24:04 +0100

    redimenssion des élément et ajout des vache voiture et gens

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Items : MonoBehaviour
 {
+
+    void Update()
+    {
+        Vector3 difference = new Vector3(0,0,0) - transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ + 90);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Finish"))
